47223b08937a254067b694b20961c4fa20a830b6
Olga Volina <olvo@isd.dp.ua>
2022-11-27 22:46:52 +0000

    spanico

---
+++ pages/index.vue
@@ -0,0 +1,76 @@
+<template>
+  <main>
+    <h1>Spanico</h1>
+    <div class="notas">
+      <div class="nota">
+        <h3 class="titulo">Leccion 1</h3>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Id natus, veniam magni ratione voluptas quod perferendis quidem, esse optio veritatis obcaecati cupiditate aliquid ipsam nemo cum quisquam eius impedit sapiente.</p>
+      </div>
+
+      <div class="nota">
+        <h3 class="titulo">Leccion 2</h3>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Id natus, veniam magni ratione voluptas quod perferendis quidem, esse optio veritatis obcaecati cupiditate aliquid ipsam nemo cum quisquam eius impedit sapiente.</p>
+      </div>
+
+      <div class="nota">
+        <h3 class="titulo">Leccion 3</h3>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Id natus, veniam magni ratione voluptas quod perferendis quidem, esse optio veritatis obcaecati cupiditate aliquid ipsam nemo cum quisquam eius impedit sapiente.</p>
+      </div>
+
+      <div class="nota">
+        <h3 class="titulo">Leccion 4</h3>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Id natus, veniam magni ratione voluptas quod perferendis quidem, esse optio veritatis obcaecati cupiditate aliquid ipsam nemo cum quisquam eius impedit sapiente.</p>
+      </div>
+
+      <div class="nota">
+        <h3 class="titulo">Leccion 4</h3>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Id natus, veniam magni ratione voluptas quod perferendis quidem, esse optio veritatis obcaecati cupiditate aliquid ipsam nemo cum quisquam eius impedit sapiente.</p>
+      </div>
+    </div>
+    <div class='button'>
+      <NuxtLink  to="/notaForm">+ anadir nota</NuxtLink>      
+    </div>
+  </main>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage'
+}
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Libre+Baskerville:ital,wght@0,400;0,700;1,400&display=swap');
+
+* {
+  font-family: 'Libre Baskerville', serif;
+  box-sizing: border-box;
+}
+
+h1 {
+  text-align: center;
+}
+
+.button {
+  text-align: right;
+  max-width: 90%;
+}
+
+.notas{
+  max-width: 90%;
+  margin: 0 auto;
+  display: flex;
+  flex-wrap: wrap;
+  gap: 3%;
+}
+
+.nota {
+  background-color:beige;
+  flex-basis: 22.5%;
+  margin-bottom: 3%;
+  padding: 5px;
+}
+
+
+
+</style>
---
+++ pages/notaForm.vue
@@ -0,0 +1,75 @@
+<template>
+    <main>
+        <h1>Anade nueva nota</h1>
+        <div class='button2'>
+            <NuxtLink to="/">&lt; volver</NuxtLink>
+        </div>
+        <form action="">
+            <div>
+                <input type="text">
+            </div>
+            <div>
+                <textarea name="" id="" cols="30" rows="10"></textarea>
+            </div>
+            <div>
+                <input type="submit" value="Vale">
+            </div>
+        </form>
+
+    </main>
+</template>
+  
+<script>
+export default {
+    name: 'notaForm'
+}
+</script>
+  
+<style>
+.button2 {
+    text-align: left;
+    max-width: 90%;
+    margin: 0 auto;
+}
+
+form {
+    display: block;
+    text-align: center;
+    max-width: 90%;
+    margin: 0 auto;
+}
+
+input[type=text]{
+    margin-top: 30px;
+    padding: 6px 12px;
+    font-size: 16px;
+    font-weight: 400;
+    line-height: 1.5;
+    color: #212529;
+    background-color: #fff;
+    background-clip: padding-box;
+    border: 1px solid #ced4da;
+    appearance: none;
+    border-radius: 4px;
+    transition: border-color .15s ease-in-out, box-shadow .15s ease-in-out;
+    min-width: 80%;
+}
+
+textarea {
+    min-width: 80%;
+    margin-top: 15px;
+    border-radius: 4px;
+}
+
+input[type="submit"] {
+    width: 10%;
+    float: right;
+    margin-right: 10%;
+    padding: 7px !important;
+    font-size: 18px;
+    border-radius: 4px;
+    background-color: gray;
+}
+
+</style>
+  
